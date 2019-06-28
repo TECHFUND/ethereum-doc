df4f0e2df86f7ae6e488d8f81115ef948ff760be
Yuta <please.please.me518@gmail.com>
2019-06-28 12:18:51 +0900

    Mintable Token

---
+++ MintToken.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract SNFCoin is ERC20, ERC20Detailed, ERC20Mintable {
+
+    string private _name = "SANFRE";
+    string private _symbol = "SNF";
+    uint8 private _decimals = 18;
+
+    address account = msg.sender;
+    uint value = 100000000000000000000000000;
+
+    constructor() ERC20Detailed( _name, _symbol, _decimals) public {
+        _mint(account, value);
+    }
+}
+
